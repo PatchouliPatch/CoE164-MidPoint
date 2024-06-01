e69507151476c3b4fe5a253a0d7b3c783b39a794
tbsalgado <83205070+tbsalgado@users.noreply.github.com>
2024-06-01 17:01:29 +0800

    Update crc_tpl.rs
    Not yet checked

--- src/flac/encoder/crc_tpl.rs
+++ src/flac/encoder/crc_tpl.rs
@@ -16,7 +16,10 @@ pub struct CrcOptions <T> {
 impl <T> CrcOptions <T> {
     /// Create a builder to the CRC encoder
     pub fn new(poly: T, poly_len: T) -> Self {
-        todo!()
+        CrcOptions {
+            poly: poly,
+            poly_len: poly_len,
+        }
     }
 }
 
@@ -25,7 +28,54 @@ impl CrcOptions <u8> {
     /// 
     /// This method is available only if `CrcOptions` is of type `u8`.
     pub fn build_crc8(&self, data: &Vec <u8>) -> u8 {
-        todo!()
+        //create divisor for XOR ops type int for easie operations
+        let divisor :i64 = ((0b1 << self.poly_len) + self.poly as i64);
+
+        //combine all data in VEC as single number
+        let mut comb_vec : i128= 0;
+        
+        for i in 0..data.len(){
+            if i == 0{
+                comb_vec = data[i] as i128;
+            }
+            else{
+                comb_vec = comb_vec + (data[i] as i128);
+            }
+            comb_vec = comb_vec << self.poly_len;
+        }
+        //count amount of bits then shift the data by poly.len number of zeroes 
+        let mut counter :u32 = comb_vec.ilog2() as u32;
+        comb_vec = comb_vec << self.poly_len;
+        //make a dividend base on comb_vec with poly_len number of bits
+        let mut dividend :i64 = (comb_vec >> (counter)) as i64;
+
+        //Divide or XOR continously 
+        while counter != 0 {
+            if dividend.ilog2() >= (self.poly_len as u32){
+                //if bits of dividend is equal to bits of divisor proceed with XOR
+                dividend = divisor ^ dividend;
+            }
+            else{
+                //else append a bit from temp to the divisor 
+                dividend = dividend << 1;//shift 1 to append 1 trailling bit 
+                let append :i64 = ((comb_vec  & (0b1 <<(counter - 1))) >> counter -1) as i64; //get the nearest trailing bit by getting the counter-1 bit
+                //combine 
+                dividend = dividend + append;
+                //take count of the added bit 
+                counter = counter - 1;
+            }
+        }
+        // at last append possibility that divident and divisor bits are still equal 
+
+        if dividend.ilog2() >= (self.poly_len as u32){
+            //if bits of dividend is equal to bits of divisor proceed with XOR
+            dividend = divisor ^ dividend;
+            return (dividend as u8)
+        }
+        else{
+            return (dividend as u8)
+        }
+
     }
 }
 
@@ -34,8 +84,55 @@ impl CrcOptions <u16> {
     /// 
     /// This method is available only if `CrcOptions` is of type `u16`.
     pub fn build_crc16(&self, data: &Vec <u16>) -> u16 {
-        todo!()
+        let divisor :i64 = ((0b1 << self.poly_len) + self.poly as i64);
+
+        //combine all data in VEC as single number
+        let mut comb_vec : i128= 0;
+        
+        for i in 0..data.len(){
+            if i == 0{
+                comb_vec = data[i] as i128;
+            }
+            else{
+                comb_vec = comb_vec + (data[i] as i128);
+            }
+            comb_vec = comb_vec << self.poly_len;
+        }
+        //count amount of bits then shift the data by poly.len number of zeroes 
+        let mut counter :u32 = comb_vec.ilog2() as u32;
+        comb_vec = comb_vec << self.poly_len;
+        //make a dividend base on comb_vec with poly_len number of bits
+        let mut dividend :i64 = (comb_vec >> (counter )) as i64;
+
+        //Divide or XOR continously 
+        while counter != 0 {
+            if dividend.ilog2() >= (self.poly_len as u32){
+                //if bits of dividend is equal to bits of divisor proceed with XOR
+                dividend = divisor ^ dividend;
+            }
+            else{
+                //else append a bit from temp to the divisor 
+                dividend = dividend << 1;//shift 1 to append 1 trailling bit 
+                let append :i64 = ((comb_vec  & (0b1 <<counter - 1)) >> counter -1) as i64; //get the nearest trailing bit by getting the counter-1 bit
+                //combine 
+                dividend = dividend + append;
+                //take count of the added bit 
+                counter = counter - 1;
+            }
+        }
+        // at last append possibility that divident and divisor bits are still equal 
+
+        if dividend.ilog2() >= (self.poly_len as u32){
+            //if bits of dividend is equal to bits of divisor proceed with XOR
+            dividend = divisor ^ dividend;
+            return (dividend as u16)
+        }
+        else{
+            return (dividend as u16)
+        }
+
     }
+    
 }
 
 #[cfg(test)]
@@ -89,4 +186,4 @@ mod tests {
 
         assert_eq!(ans, 0xaa9a);
     }
-}
+}    
